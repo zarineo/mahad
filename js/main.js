0219bd81e13f0de2c30f9c87771160e924da7e45
zarineo <tochilka007@mail.ru>
2024-08-09 17:50:11 +0300

    отзывы

--- js/main.js
+++ js/main.js
@@ -3,9 +3,29 @@
       const subMenu = this.querySelector('.items');
       if (subMenu) {
         // e.preventDefault(); // Предотвращает переход по ссылке
-
         subMenu.classList.toggle('active');
-     
       }
     });
   });
+
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    
+    slidesPerView: 1,
+    mousewheel: {
+      forceToAxis: true,
+    },
+  
+  });
+  console.log('hello');
