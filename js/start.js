a62d9c24ea90889ac1d36716ad4c7fc40d3a5e34
zarineo <tochilka007@mail.ru>
2024-08-26 12:45:46 +0300

    исправления

--- js/start.js
+++ js/start.js
@@ -27,9 +27,11 @@
     success.classList.remove('modal-active');
     formModal.classList.remove('modal-active');
     backdropDark.classList.remove('is-active');
+    console.log('close');
+
   }
 
-  sendData.addEventListener('click', openSuccess);
+  // sendData.addEventListener('click', openSuccess);
 
   successButtons.forEach((successButton) => {
     successButton.addEventListener('click', closeSuccess);
@@ -41,4 +43,41 @@
     backdropDark.addEventListener('click', closeModal);
   })
 
-  
+
+  document.querySelector('.form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Проверка валидности формы
+    if (this.checkValidity()) {
+        // Закрываем текущее модальное окно
+        document.querySelector('.form__modal').classList.remove('modal-active');
+
+        // Открываем окно успеха
+        document.querySelector('.success').classList.add('modal-active');
+
+        const formData = new FormData(this);
+        const data = {
+            chat_id: "462291550",  
+            text: `Фамилия Имя: ${formData.get('userName')}\nВозраст: ${formData.get('userAge')}\nСтрана проживания: ${formData.get('userCountry')}\nТелеграм: ${formData.get('userTelegram')}\nEmail: ${formData.get('userMail')}\nТемп обучения: ${formData.get('userGrafic')}\nУровень чтения Корана: ${formData.get('userKoranLvl')}\nУровень владения арабским языком: ${formData.get('userArabLvl')}`
+        };
+
+        fetch(`https://api.telegram.org/bot6452584114:AAERbOG2WM1dQqVveN6BkS2hUDIyL1k0gAw/sendMessage`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }).then(response => {
+            if (response.ok) {
+                console.log('Сообщение успешно отправлено в Telegram');
+            } else {
+                console.error('Ошибка при отправке сообщения в Telegram:', response.statusText);
+            }
+        }).catch(error => {
+            console.error('Ошибка отправки формы:', error);
+        });
+
+    } else {
+        this.reportValidity();
+    }
+});
